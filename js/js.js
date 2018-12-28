3fde6cb4d68be7339d0b5a32b5fc63e601427eba
Md Ashikur Rahaman <46211523+aashikur@users.noreply.github.com>
2018-12-28 18:10:29 +0600

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	
+	
+  $('#menu li').click(function(){
+    $('#menu li').removeClass("active");
+    $(this).addClass("active");
+});
+
+
+
+
+
+
+$('.continue_r').click(function(){
+	alert('working on it..');
+	return false;
+});
+
+
+
+
+
+});
